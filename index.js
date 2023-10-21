7562cf6f29a8c93314d5c7bb858fd7e65666c517
mc1802 <marvin.ckr@gmail.com>
2023-10-21 22:21:01 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+const apiKey = "76ad6ef1080b2cf4ac35cf063a515dab";
+const searchBox = document.querySelector(".search-box");
+const wind = document.querySelector(".wind >p");
+const humidity = document.querySelector(".humidity >p");
+const conditionImage = document.querySelector(".weather >img");
+const conditionLogos = {
+    Clouds:"media/clouds.png",
+    Clear:"media/clear.png",
+    Drizzle:"media/drizzle.png",
+    Mist:"media/mist.png",
+    Rain:"media/rain.png",
+    Snow:"media/snow.png",
+}
+
+async function checkWeather(cityName){
+    const response = await fetch(`${apiUrl}${cityName}&appid=${apiKey}&units=metric&lang=en`);
+    let data = await response.json();
+    if(data.cod==="404"){
+        alert("City not found");
+        return;  
+    }
+    
+    var condition = data.weather[0].main;
+    conditionImage.src = conditionLogos[condition];
+
+    document.querySelector(".city-name").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp)+"°C";
+    wind.innerHTML = data.wind.speed + " km/h";
+    humidity.innerHTML = data.main.humidity + " %"; 
+}
+
+
+function searchCity(){
+    if (searchBox.value===""){
+        alert("Enter Valid City");
+    }
+    else{
+        checkWeather(searchBox.value);
+        searchBox.value="";
+    }
+}
+
+function handleKeyPress(event) {
+
+    if (event.key === 'Enter') {
+      
+      searchCity()
+    }
+  }
+  
+  
+  document.addEventListener('keydown', handleKeyPress);
+
